aba19a1f99c95f7a267c2c131157d8f4546d8366
Crowdbotics <team@crowdbotics.com>
2021-09-05 10:49:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/jhoij_30326/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jhoij_30326;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "jhoij_30326";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "jhoij_30326Identifier",
+  url: "https://jhoij_30326Identifier.botics.co",
+  api: "https://jhoij_30326Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
